7cafaafaefb60f334561969f40614c55c895da78
saiswetha.v <saiswetha0524@gmaill.com>
2023-12-30 09:45:23 +0530

    implemented interceptor

---
+++ src/main/java/com/example/StudentApplication/configuration/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.StudentApplication.configuration;
+
+import com.example.StudentApplication.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+    }
+}
