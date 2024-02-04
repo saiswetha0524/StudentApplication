250b707e8d31d014db173f1e9dbc162e402814e3
saiswetha.v <saiswetha0524@gmaill.com>
2024-02-04 12:56:11 +0530

    added docker file

--- src/main/java/com/example/StudentApplication/controller/StudentController.java
+++ src/main/java/com/example/StudentApplication/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.example.StudentApplication.controller;
 
+import com.example.StudentApplication.models.APIResponse;
 import com.example.StudentApplication.service.ServiceHelper;
 import org.openapitools.api.StudentDetailsApi;
+import org.openapitools.model.ModelAPIResponse;
 import org.openapitools.model.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/studentDetails")
@@ -18,46 +24,50 @@ public class StudentController implements org.openapitools.api.StudentDetailsApi
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 
     @Override
-    @PostMapping("/")
-    public ResponseEntity<Student> additionOfStuds(@RequestBody Student student) {
-       return StudentDetailsApi.super.additionOfStuds(student);
+    public Optional<NativeWebRequest> getRequest() {
+        return StudentDetailsApi.super.getRequest();
     }
 
     @Override
-    @GetMapping("/count")
-    public ResponseEntity<Student> countOfStuds() {
+    public ResponseEntity<Student> additionOfStuds(Student student) {
+        return StudentDetailsApi.super.additionOfStuds(student);
+    }
+
+    @Override
+    public ResponseEntity<Long> countOfStuds() {
         return StudentDetailsApi.super.countOfStuds();
     }
 
     @Override
-    @DeleteMapping("/")
-    public ResponseEntity<Student> delAllStuds() {
+    public ResponseEntity<ModelAPIResponse> delAllStuds() {
         return StudentDetailsApi.super.delAllStuds();
     }
 
     @Override
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Student> delStudWithId(Integer id) {
+    public ResponseEntity<ModelAPIResponse> delStudWithId(Integer id) {
         return StudentDetailsApi.super.delStudWithId(id);
     }
 
     @Override
-    @GetMapping("/")
-    public ResponseEntity<Student> getAllStuds() {
+    public ResponseEntity<ModelAPIResponse> getAllStuds() {
         return StudentDetailsApi.super.getAllStuds();
     }
 
     @Override
-    @GetMapping("/{id}")
-    public ResponseEntity<Student> getStudWithId(Integer id) {
-        return StudentDetailsApi.super.getStudWithId(id);
+    public ResponseEntity<ModelAPIResponse> getStudWithId(Integer id) {
+        logger.trace("Received GET request at /{} endpoint to fetch student by id at trace level", id);
+        logger.info("Received GET request at /{} endpoint to fetch student by id at info level", id);
+        return serviceHelper.findById(id);
+        //return StudentDetailsApi.super.getStudWithId(id);
     }
 
     @Override
-    @GetMapping("/hi")
-    public ResponseEntity<Student> welcomeAll() {
-        return StudentDetailsApi.super.welcomeAll();
+    public ResponseEntity<String> welcomeAll() {
+        logger.info("Received GET request at /hi endpoint");
+        return ResponseEntity.status(HttpStatus.OK).body(serviceHelper.helloMethod());
+       // return StudentDetailsApi.super.welcomeAll();
     }
+
 /*@GetMapping("/hi")
     public ResponseEntity<String> hello() {
         logger.info("Received GET request at /hi endpoint");
